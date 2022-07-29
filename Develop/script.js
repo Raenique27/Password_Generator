c71902a6ccd07f5a0e6fad57cf00aa41009f4ddd
Raenique27 <raeniquewalters@gmail.com>
2022-07-28 23:21:54 -0400

    add window prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,45 @@
 // Assignment code here
+ var passwordCriteria = {
+    Lowers: "bcagfejihponmlsqrutvxwzy",
+    Uppers: "BCAGFEJIHPONMLSQRUTVXWZY",
+    Numbers: "1234567890",
+    Characters: "!#@$*^&%"
+
+   }
+
+function writePasswordPrompt () {
+  var passwordLengthPrompt = window.prompt("Choose length of password !");
+  passwordLengthPrompt= parseInt(passwordLengthPrompt);
+  
+  if (passwordLengthPrompt < 8) {
+    alert("Password length needs to be at least 8 characters. Please choose length of password again !");
+    return writePasswordPrompt();
+
+  } else if (passwordLengthPrompt > 128) {
+      alert("Password length cannot be more than 128 characters. Please choose length of password again !");
+      return writePasswordPrompt();
+
+    }
+
+  console.log(passwordLengthPrompt);
+};
+
+function passwordCharacterTypes () {
+  var confirmCharacters = window.confirm ("Would you like you like to include lowercase, uppercase, numeric, and/or special characters ?");
+
+  if (confirmCharacters === "Ok") {
+      
+  }
+  
+}
+
+
+function generatePassword() {
+  writePasswordPrompt();
+  passwordCharacterTypes();
+}
+
+
 
 
 // Get references to the #generate element
@@ -11,7 +52,14 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+  
+  
+
+
+
+
